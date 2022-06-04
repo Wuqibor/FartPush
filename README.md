ba40f5f836abc69bec10944f5da163061eff5c61
Wuqibor <77870838+Wuqibor@users.noreply.github.com>
2022-06-04 23:35:40 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,45 @@
+# 钉钉土味情话推送
+每日通过钉钉定时推送一段土味情话
+
+使用方法  
+1.测试环境为 Python3.7.6 , 自行安装 Python3  
+2.requirements.txt 是所需第三方模块, 执行 `pip install -r requirements.txt` 安装模块  
+3.可在脚本内直接填写账号密码  
+4.Python 和需要模块都装好了直接在目录 cmd 运行所要运行的脚本。  
+
+# Github Actions说明
+## 一、Fork此仓库
+![](http://tu.yaohuo.me/imgs/2020/06/f059fe73afb4ef5f.png)
+## 二、设置账号密码和钉钉推送
+添加名为**DINGTOKEN**、**DINGSECRET**的变量  
+值分别为**钉钉推送密钥**、**钉钉推送加签**  
+钉钉推送支持加签模式, 如不需加签留空即可
+示例：**DINGTOKEN:wy729c4z52x56acouvk7m8p2inlvldl3qezfpbnjxw7eidonsmf2q9x1gebpgegu**, **DINGSECRET:SEC32j3vxb30umqqcfq27k8d72dfxsjhz0tpcjd36wqg80os8x4ejfqydocvl7sbgee**  
+![](http://tu.yaohuo.me/imgs/2020/06/748bf9c0ca6143cd.png)
+
+## 三、启用Action
+1 点击**Action**, 再点击**I understand my workflows, go ahead and enable them**  
+2 修改任意文件后提交一次  
+![](http://tu.yaohuo.me/imgs/2020/06/34ca160c972b9927.png)
+
+## 四、查看运行结果
+Actions > FartPush > build  
+能看到类似如下所示:
+```
+### info There are 1 user | 共有 1 个账号 ###
+### info Enable DingTalk push | 启用钉钉推送 ###
+### info No. 1 user starting | 开始第 1 个账号 ###
+### info Login *** successful | 登录 *** 成功 ###
+### info *** chunckined , get 17 M | *** 已签到, 获得 17 M ###
+### error #4 *** hava not chance  | *** 无机会 ###
+### error #4 *** hava not chance  | *** 无机会 ###
+天翼云签到
+开始时间: 2022-05-29 04:37:09
+结束时间: 2022-05-29 04:37:23
+账号 签到结果 抽奖1结果 抽奖2结果
+*** 已签到17M 无机会 无机会
+### info DingTalk push successfully  | 钉钉推送成功 ###
+```
+
+此后, 将会在每天08:00和22:00推送一次 
+若有需求, 可以在[.github/workflows/push.yml]中自行修改
---
+++ push.py
@@ -0,0 +1,72 @@
+import argparse
+import base64
+import hmac
+import json
+import requests
+import time
+import urllib.parse
+import hashlib
+
+
+# 钉钉推送 加签 text模式 # 标题 消息 token 签名密钥 是否艾特全体 # By Wuqibor
+def ding(title, msg, token, secret, isAtAll=True):
+    # 推送
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2486.0 Safari/537.36 Edge/13.10586",
+        "Content-Type": "application/json"
+    }
+    msgjson = {
+        "msgtype": "text",
+        "text": {
+            "title": title,
+            "content": msg + "\n"
+        },
+        "at": {
+            "isAtAll": isAtAll
+        }
+    }
+    dingApi = "https://oapi.dingtalk.com/robot/send?"
+    pushUrl = dingApi + "access_token=" + token
+
+    if secret != "":  # 是否需要加签
+        # 钉钉推送签名算法
+        timestamp = str(round(time.time() * 1000))
+        secret_enc = secret.encode('utf-8')
+        string_to_sign = '{}\n{}'.format(timestamp, secret)
+        string_to_sign_enc = string_to_sign.encode('utf-8')
+        hmac_code = hmac.new(secret_enc, string_to_sign_enc, digestmod=hashlib.sha256).digest()
+        sign = urllib.parse.quote_plus(base64.b64encode(hmac_code))
+        pushUrl = pushUrl + "&timestamp=" + timestamp + "&sign=" + sign
+
+    rs = requests.post(url=pushUrl, data=json.dumps(msgjson), headers=headers)
+    return str(rs.text)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='钉钉土味情话推送')
+    parser.add_argument('--dingtoken', type=str, help='钉钉token')
+    parser.add_argument('--dingsecret', type=str, help='钉钉secret')
+    args = parser.parse_args()
+    dingtoken = args.dingtoken
+    dingsecret = args.dingsecret
+    if dingtoken == "":
+        print("### error DingTalk push token not found | 未发现钉钉推送Token ###")
+        exit(3)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36 Edg/102.0.1245.30",
+        "Host": "api.lovelive.tools"
+    }
+
+    api = "http://api.lovelive.tools/api/SweetNothings"
+    text = requests.get(url=api, headers=headers, timeout=60)
+
+    if text.ok:
+        print(f"### info text: {text.text} ###")
+        json1 = json.loads(ding("日日夜夜土味情话", text.text, dingtoken, dingsecret, False))
+        if str(json1['errcode']) == "0":
+            print("### info DingTalk push successfully | 钉钉推送成功 ###")
+        else:
+            print(f"### error DingTalk push falied | 钉钉推送失败 : {json.dumps(json1)} ###")
+    else:
+        print(f"### error Failed to obtain unfashionable love words | Failed to obtain unfashionable love words ###")
